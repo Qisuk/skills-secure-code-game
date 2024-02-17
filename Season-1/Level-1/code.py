f6990625539aa7f139d68c3a419bf7ee32f7e479
Qisuk <Chris.moir@gmail.com>
2024-02-17 12:34:28 +0000

    season1-level1 now passing, float overflows and input validation- TODO: refactor

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -12,22 +12,42 @@ Follow the instructions below to get started:
 '''
 
 from collections import namedtuple
+from decimal import Decimal
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+
+max_order_value = 10_000
+max_payment_value = 100_000
+
 def validorder(order: Order):
+    total_charged=0
+    total_paid=0
     net = 0
 
     for item in order.items:
+        print(item)
         if item.type == 'payment':
-            net += item.amount
+            if ((item.amount >=(max_payment_value*-1)) & (item.amount <= max_payment_value)):
+                total_paid += Decimal(str(item.amount))
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            product_total = Decimal(str(item.amount)) *  Decimal(str(item.quantity))
+            print(f"product_total:{product_total}")
+            total_charged += product_total
         else:
             return "Invalid item type: %s" % item.type
+        print(f"{order.id} : net {net}")
+    net = total_paid-total_charged
 
+    if ( (max_order_value *-1)  > total_paid) | (total_paid > (max_order_value )):
+        return 'Total amount payable for an order exceeded'
+
+    print(f"total = {net}")
     if net != 0:
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
+    if ( (max_order_value *-1)  > net) | (net > (max_order_value )):
+        return 'Total amount payable for an order exceeded'
     else:
         return "Order ID: %s - Full payment received!" % order.id
+    
